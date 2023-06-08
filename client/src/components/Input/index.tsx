000c5633a36dbec4f218d615fb12da66deebe307
MyeongHoonNam <codenmh0822@naver.com>
2023-06-08 18:21:00 +0900

    feat : Input 컴포넌트 style 구현

--- client/src/components/Input/index.tsx
+++ client/src/components/Input/index.tsx
@@ -2,7 +2,13 @@ import type { InputProps } from './types';
 
 const Input = ({ value, onChange, testId }: InputProps) => {
   return (
-    <input type="text" value={value} onChange={onChange} data-testid={testId} />
+    <input
+      type="text"
+      value={value}
+      onChange={onChange}
+      data-testid={testId}
+      className="w-[250px] h-[30px] text-[14px] border-[1px] border-[grey] px-[4px] py-[2px] rounded-[2px] outline-none"
+    />
   );
 };
 
