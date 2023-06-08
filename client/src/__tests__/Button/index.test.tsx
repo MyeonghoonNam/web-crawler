52a58683581a766ac173cc05db094f2524a31b98
MyeongHoonNam <codenmh0822@naver.com>
2023-06-08 18:35:15 +0900

    test : Button 컴포넌트 render 테스트

---
+++ client/src/__tests__/Button/index.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+
+import { Button } from '@/components';
+import { PAGE_URL_SUBMIT_BUTTON_TEST_ID } from '@/constants/Button';
+
+describe('Button Component', () => {
+  it('render', () => {
+    const text = 'SUBMIT';
+    const testId = PAGE_URL_SUBMIT_BUTTON_TEST_ID;
+
+    render(<Button text="SUBMIT" testId={testId} />);
+
+    const button = screen.getByTestId(testId);
+
+    expect(button).toBeInTheDocument();
+    expect(button.textContent).toBe(text);
+  });
+});
