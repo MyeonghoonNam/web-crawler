61c18a4366991182dfaccbeb4b48ac85f5f7dabb
MyeongHoonNam <codenmh0822@naver.com>
2023-06-08 18:21:32 +0900

    feat : Input 컴포넌트 storybook onChange action 구현

--- client/src/stories/Input.stories.tsx
+++ client/src/stories/Input.stories.tsx
@@ -1,10 +1,14 @@
+import type { StoryFn } from '@storybook/react';
 import { Input } from '@/components';
 
 export default {
   title: 'Components/Input',
   component: Input,
+  argTypes: {
+    onChange: { action: 'onChange' },
+  },
 };
 
-export const Default = () => {
-  return <Input />;
+export const Default: StoryFn<typeof Input> = (args) => {
+  return <Input {...args} />;
 };
