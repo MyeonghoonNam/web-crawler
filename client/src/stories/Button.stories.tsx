89b0cb8868fcfa3f4b9052fa0e748b088d9e2630
MyeongHoonNam <codenmh0822@naver.com>
2023-06-09 18:26:42 +0900

    feat : Button 컴포넌트 스토리북 구현

---
+++ client/src/stories/Button.stories.tsx
@@ -0,0 +1,14 @@
+import type { StoryFn } from '@storybook/react';
+import { Button } from '@/components';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    onClick: 'onClick',
+  },
+};
+
+export const Default: StoryFn<typeof Button> = (args) => {
+  return <Button {...args}>SUBMIT</Button>;
+};
