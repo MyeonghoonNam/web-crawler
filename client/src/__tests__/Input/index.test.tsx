08ab48f079bb2d5decaa8552f23af53ea38468a3
MyeongHoonNam <codenmh0822@naver.com>
2023-06-14 15:56:30 +0900

    test : Input 컴포넌트 testId 상수 적용

--- client/src/__tests__/Input/index.test.tsx
+++ client/src/__tests__/Input/index.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 
 import { Input } from '@/components';
+import { PAGE_URL_INPUT_TEST_ID } from '@/constants/Input';
 
 describe('Input Component', () => {
   it('render', () => {
@@ -14,13 +15,13 @@ describe('Input Component', () => {
   });
 
   it('is input have value ', async () => {
-    const testId = 'Test';
+    const testId = PAGE_URL_INPUT_TEST_ID;
     const value = 'Test Value';
     const onChange = jest.fn();
 
     render(<Input value={value} onChange={onChange} testId={testId} />);
 
-    const input: HTMLInputElement = screen.getByTestId(testId);
+    const input = screen.getByTestId(testId);
 
     expect(input).toHaveValue(value);
   });
