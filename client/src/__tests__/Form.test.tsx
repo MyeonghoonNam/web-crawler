4ec22f59cbd8b9bed05c2025467af42bea68a841
MyeongHoonNam <codenmh0822@naver.com>
2023-06-28 17:26:08 +0900

    test : input 값 비었을경우의 예외처리 테스트

--- client/src/__tests__/Form.test.tsx
+++ client/src/__tests__/Form.test.tsx
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import { Form } from '@/components';
 import userEvent from '@testing-library/user-event';
+import { Form } from '@/components';
+import { FORM_URL_INPUT_EMPTY_VALUE } from '@/constants/errors';
 
 describe('Form Component', () => {
-  it('input value onChange works correctly', async () => {
+  it('input 값 변경이 제대로 이루어지는가?', async () => {
     render(<Form />);
 
     const urlInputLabelText = 'URL';
@@ -13,5 +14,18 @@ describe('Form Component', () => {
     await userEvent.type(urlInput, urlInputvalue);
 
     expect(urlInput).toHaveValue(urlInputvalue);
+  });
+
+  it('input 값이 비었을 때 폼 제출 버튼을 클릭하였을 경우 오류 텍스트가 표시되는가?', async () => {
+    render(<Form />);
+
+    const submitButtonText = 'SUBMIT';
+    const submitButton = screen.getByText(submitButtonText);
+
+    await userEvent.click(submitButton);
+
+    const urlErrorText = screen.getByText(FORM_URL_INPUT_EMPTY_VALUE);
+
+    expect(urlErrorText).toBeInTheDocument();
   });
 });
