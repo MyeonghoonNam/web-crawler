45b1c10146e03e48ff589e1b01c254ce0f708c99
MyeongHoonNam <codenmh0822@naver.com>
2023-07-07 17:59:51 +0900

    refactor : 동적 객체 형태 상태의 Form 컴포넌트 수정

--- client/src/components/Form/index.tsx
+++ client/src/components/Form/index.tsx
@@ -1,24 +1,64 @@
+import { ChangeEvent, useState, useCallback } from 'react';
 import { useForm } from '@/hooks';
 import { FORM_URL_INPUT_EMPTY_VALUE } from '@/constants/errors';
 
+type FormState = {
+  url: string;
+  container: string;
+  dictionary: {
+    [key: string]: string;
+  }[];
+};
+
 const Form = () => {
-  const { errors, handleChange, handleSubmit } = useForm({
-    initialState: [{ url: '' }, { container: '' }, { key: '', value: '' }],
-    validate: ({ url }) => {
-      let urlError = '';
-
-      if (!url) {
-        urlError = FORM_URL_INPUT_EMPTY_VALUE;
-      }
-
-      return {
-        url: urlError,
-      };
-    },
-    onSubmit: async () => {
-      //
-    },
+  const [initialState, setInitialState] = useState<FormState>({
+    url: '',
+    container: '',
+    dictionary: [{ key0: '', value0: '' }],
   });
+
+  const [nextKeyValueIndex, setNextKeyValueIndex] = useState(1);
+
+  const handleChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>, index?: number) => {
+      const { name, value } = e.target;
+
+      if (index) {
+        setInitialState((state) => {
+          const newState = { ...state };
+
+          newState.dictionary[index][name] = value;
+
+          return newState;
+        });
+      } else {
+        setInitialState((state) => ({ ...state, [name]: value }));
+      }
+    },
+    [],
+  );
+
+  const handleSubmit = useCallback(() => {
+    console.log('submit');
+  }, []);
+
+  const handleAddButtonClick = useCallback(() => {
+    setInitialState((state) => {
+      const dictionary = [
+        ...state.dictionary,
+        {
+          [`key${nextKeyValueIndex}`]: '',
+          [`value${nextKeyValueIndex}`]: '',
+        },
+      ];
+
+      const newState = { ...state, dictionary };
+
+      return newState;
+    });
+
+    setNextKeyValueIndex((state) => state + 1);
+  }, [nextKeyValueIndex]);
 
   return (
     <form onSubmit={handleSubmit}>
@@ -31,14 +71,56 @@ const Form = () => {
           onChange={handleChange}
           className="w-[250px] h-[30px] text-[14px] border-[1px] border-[grey] px-[4px] py-[2px] rounded-[2px] outline-none"
         />
-        <span>{errors?.url}</span>
       </div>
+
+      <div>
+        <label htmlFor="container">Container</label>
+        <input
+          type="text"
+          id="container"
+          name="container"
+          onChange={handleChange}
+          className="w-[250px] h-[30px] text-[14px] border-[1px] border-[grey] px-[4px] py-[2px] rounded-[2px] outline-none"
+        />
+      </div>
+
+      {initialState.dictionary.map((filed, index: number) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <div key={index}>
+          <label htmlFor={`key${index}`}>Key</label>
+          <input
+            type="text"
+            id={`key${index}`}
+            name={`key${index}`}
+            onChange={(e) => handleChange(e, index)}
+            className="w-[250px] h-[30px] text-[14px] border-[1px] border-[grey] px-[4px] py-[2px] rounded-[2px] outline-none"
+          />
+
+          <label htmlFor={`value${index}`}>Value</label>
+          <input
+            type="text"
+            id={`value${index}`}
+            name={`value${index}`}
+            onChange={(e) => handleChange(e, index)}
+            className="w-[250px] h-[30px] text-[14px] border-[1px] border-[grey] px-[4px] py-[2px] rounded-[2px] outline-none"
+          />
+        </div>
+      ))}
 
       <button
         type="submit"
+        onClick={handleSubmit}
         className="w-[100px] h-[40px] text-[white] bg-[black] rounded-[5px]"
       >
         SUBMIT
+      </button>
+
+      <button
+        type="button"
+        onClick={handleAddButtonClick}
+        className="w-[100px] h-[40px] text-[white] bg-[black] rounded-[5px]"
+      >
+        Add
       </button>
     </form>
   );
