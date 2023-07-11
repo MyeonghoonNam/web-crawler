de16467c72b0f6d826e9836d9fd3fc44e252a6c7
MyeongHoonNam <codenmh0822@naver.com>
2023-07-11 20:29:36 +0900

    refactor : useRef를 활용한 동적 input submit 로직 수정

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,16 +1,51 @@
 import { Input } from '@/components';
+import { useState, useCallback, useRef, FormEvent } from 'react';
+
+import uuid from 'react-uuid';
 
 const Home = () => {
+  const [formFiled, setFormFiled] = useState([
+    { label: 'URL', name: 'url', value: '' },
+    { label: 'Container', name: 'container', value: '' },
+    { label: 'Key', name: 'key', value: '' },
+    { label: 'Value', name: 'value', value: '' },
+  ]);
+
+  const inputRefArr = useRef<(HTMLInputElement | null)[]>([]);
+
+  const handleSubmit = useCallback((e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const inputValues = [];
+
+    for (let i = 0; i < inputRefArr.current.length; i += 1) {
+      const target = inputRefArr.current[i];
+      const value = {
+        key: target?.name,
+        value: target?.value,
+      };
+
+      inputValues.push(value);
+    }
+
+    console.log(inputValues);
+  }, []);
+
   return (
     <main>
-      <form>
-        <Input label="URL">
-          <Input.TextField />
-        </Input>
+      <form onSubmit={handleSubmit}>
+        {formFiled.map(({ label, name }, index) => (
+          <Input label={label} key={uuid()}>
+            <Input.TextField
+              type="text"
+              name={name}
+              ref={(el) => {
+                inputRefArr.current[index] = el;
+              }}
+            />
+          </Input>
+        ))}
 
-        <Input label="Container">
-          <Input.TextField />
-        </Input>
+        <button type="submit">Submit</button>
       </form>
     </main>
   );
